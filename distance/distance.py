582f9c69b1f709181495c1b5b13bd20f45a8eb16
Stuart Walker <stuartw1@gmail.com>
2025-04-15 17:30:30 +0100

    reformat text

--- distance/distance.py
+++ distance/distance.py
@@ -1,43 +1,61 @@
 #!/usr/bin/python3
 
 #
-#                    |
-#                   >9<               Motivation
-#               8    |    0           ==========
-#                    |
-#            7       |       1        - We have a bike lock with N wheels. Each wheel has ten numbers '0' to '9' (like the diagram to the left).
-#                    X                - Moving a single wheel one position counts as one move.
-#            6               2        - How can we get between two codes (say) 9218.... and 2574.... in the fewest moves in total?
-#                           ^^^
-#               5         3           - The total sum of moves is equal to the sum of the moves required per wheel.
-#                    4                - Note that we can move each wheel either left or right.
-#                                     - We can see one move left or right is like adding or subtracting 1.
-#                                     - We perform our moves modulo 10 - e.g. 9 + 1 == 0 and 0 - 1 == 9.
+#                  |
+#                 >9< 
+#             8    |    0  
+#                  |
+#          7       |       1        
+#                  X                
+#          6               2        
+#                         ^^^
+#             5         3           
+#                  4                
 #
-#                                     - Note that the length of the route from where you start (eg '9' in diagram) to the end ('2' in diagram)
-#                                       is different depending on which way you go (clockwise or anticlockwise).
-#                                     - In this case '2' is three positions clockwise from '9' or seven positions anticlockwise.
-#                                       9 => 0 => 1 => 2 clockwise or 9 => => 8 => 7 => 6 => 5 => 4 => 3 => 2 anticlockwise.
-#                                     - Note that both of our routes should add up to 0 or 10  (==0 modulo 10).
-#                                     - We could either take the other route to get back to the starting point,
-#                                       or alternatively we never moved (we were at '9 and wanted to get to '9').
+#      Motivation
+#     ============
+#    - We have a bike lock with N wheels. Each wheel has ten numbers '0' to '9'.
+#      (like the diagram above).
+#    - Moving a single wheel one position counts as one move.
+#    - How can we get between two codes (say) 9218.... and 2574....
+#      in the fewest moves in total?
+#    - The total sum of moves is equal to the sum of the moves required per wheel.
+#    - Note that we can move each wheel either left or right.
+#    - We can see one move left or right is like adding or subtracting 1.
+#    - We perform our moves modulo 10 - e.g. 9 + 1 == 0 and 0 - 1 == 9.
 #
-#                                     Approach
-#                                     =========
-#                                     - Implement distance function d(x,y) for a single wheel as a reusable function.
-#                                     - Loop over from wheel 1 to wheel N.
-#                                     - A 'wheel' in this example is like a digit in the question.
+#    - Note that the length of the route from where you start from
+#      (eg '9' in diagram) to where you finish ('2' in diagram) is different
+#      depending on which way you go (clockwise or anticlockwise).
+#    - In this case '2' is three positions clockwise from '9'
+#      (9 => 0 => 1 => 2)
+#      or seven positions anticlockwise.
+#      (9 => => 8 => 7 => 6 => 5 => 4 => 3 => 2)
+#    - Note that both of our routes should add up to 0 or 10 (==0 modulo 10).
+#    - We could either take the other route to get back to the starting point,
+#      or alternatively we never moved (we were at '9 and wanted to get to '9').
 #
-#                                     Extension
-#                                     =========
-#                                     - The wheel or digit in this question behaves like the quotient group Z/10Z (set of integers Z modulo 10).
-#                                     - You probably don't need to know about groups before the first year of university in a math based subject.
-#                                     - If interested you could look at https://vi.wikipedia.org/wiki/Nh%C3%B3m_th%C6%B0%C6%A1ng or ask your
-#                                       math teacher or read an "Introduction to group theory textbook". I remember a little bit but my memory
-#                                       isn't that good and I would have to study it again to explain it well. Maybe YouTube is also good.
-#                                     - There are lots of programming situations where you can learn about a structure or algorithm as a tool,
-#                                       spot a pattern in your problem that looks similar to the tool and then use the structure or algorithm as
-#                                       a building block to solve part of the problem or modify it into an easier one.
+#      Approach
+#     ==========
+#    - Implement distance function d(x,y) for a single wheel as a reusable function.
+#    - Loop over from wheel 1 to wheel N.
+#    - A 'wheel' in this example is like a digit in the question.
+#
+#      Extension
+#     ===========
+#    - The wheel or digit in this question behaves like the quotient group Z/10Z
+#      (set of integers Z modulo 10).
+#    - You probably don't need to know about groups before the first year of
+#      university in a math based subject.
+#    - If interested you could look at:
+#      https://vi.wikipedia.org/wiki/Nh%C3%B3m_th%C6%B0%C6%A1ng or ask your
+#      math teacher or read an "Introduction to group theory textbook".
+#      I remember a little bit but my memory isn't that good and I would have to
+#      study it again to explain it well. Maybe YouTube is also good.
+#    - There are lots of programming situations where you can learn about a 
+#      structure or algorithm as a tool, spot a pattern in your problem that looks
+#      similar to the tool and then use the structure or algorithm as a building
+#      block to solve part of the problem or modify it into an easier one.
 
 
 
